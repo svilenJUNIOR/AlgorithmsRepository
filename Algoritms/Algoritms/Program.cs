367670401b807bc665ca71e89d36dd36e97e9914
svilen iordanov <svilenborianov@gmail.com>
2022-05-24 12:19:43 +0200

    Binary search part 3

---
+++ Algoritms/Algoritms/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Algoritms
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            BinarySearch();
+        }
+
+        static void BinarySearch()
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 20; i++) numbers.Add(i);
+
+            int first = 0;
+            int last = numbers.Count() - 1;
+            int x = 5;
+            bool found = false;
+
+            while (first <= last)
+            {
+                int midPoint = first + (last - first) / 2;
+
+                if (numbers[midPoint] == x)
+                {
+                    found = true;
+                    break;
+                }
+
+                else if (x < midPoint)
+                    last = midPoint - 1;
+
+                else if (x > midPoint)
+                    first = midPoint + 1;
+            }
+
+            if (found) Console.WriteLine("Number Found!");
+            else Console.WriteLine("Number does not exists!");
+        }
+    }
+}
