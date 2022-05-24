75f37e0ab18af8a235cc6a30ee5087c1e06c906f
svilen iordanov <svilenborianov@gmail.com>
2022-05-24 12:11:31 +0200

    Binary search part 1

---
+++ Algorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            
+        }
+
+        static void BinarySearch()
+        {
+            List<int> numbers = new List<int>();
+
+            for(int i = 0; i < 20; i++) numbers.Add(i);
+
+            int left = 0;
+            int right = numbers.Count() - 1;
+            int midPoint = left + (right - left) / 2;
+
+
+        }
+    }
+}
